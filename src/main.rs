ef53c00c95502ae7d01422cadd2226d5481392e5
Andrew Dirksen <andrew@dirksen.com>
2018-06-12 08:39:26 -0700

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 extern crate kiss3d;
-extern crate stl_io;
 extern crate nalgebra as na;
 extern crate notify;
+extern crate stl_io;
 
-use notify::{RecursiveMode, Watcher, watcher};
+use notify::{watcher, RecursiveMode, Watcher};
 use std::sync::mpsc::channel;
 use std::time::Duration;
 
-use na::{Point3, Vector3};
-use std::rc::Rc;
-use std::cell::RefCell;
-use kiss3d::window::Window;
 use kiss3d::light::Light;
 use kiss3d::resource::Mesh;
 use kiss3d::scene::SceneNode;
+use kiss3d::window::Window;
+use na::{Point3, Vector3};
+use std::cell::RefCell;
 use std::fs::File;
 use std::path::Path;
+use std::rc::Rc;
 
 fn load_stl(filename: &Path) -> stl_io::IndexedMesh {
     let mut f = File::open(&filename).expect("file not found");
@@ -26,31 +26,75 @@ fn to_resized_kiss_mesh(imesh: &stl_io::IndexedMesh) -> Mesh {
     let bounds = get_bounds(imesh);
     let center = get_center(bounds);
     let scale = get_appropriate_scale(bounds);
-    let vertices = imesh.vertices.iter()
-        .map(|v| Point3::new((v[0] - center.x) * scale, (v[1] - center.y) * scale, (v[2] - center.z) * scale))
+    let vertices = imesh
+        .vertices
+        .iter()
+        .map(|v| {
+            Point3::new(
+                (v[0] - center.x) * scale,
+                (v[1] - center.y) * scale,
+                (v[2] - center.z) * scale,
+            )
+        })
         .collect();
-    let indices = imesh.faces.iter()
+    let indices = imesh
+        .faces
+        .iter()
         .map(|it| {
-            Point3::new(it.vertices[0] as u32,it.vertices[1] as u32,it.vertices[2] as u32)
-        }).collect();
+            Point3::new(
+                it.vertices[0] as u32,
+                it.vertices[1] as u32,
+                it.vertices[2] as u32,
+            )
+        })
+        .collect();
     Mesh::new(vertices, indices, None, None, false)
 }
 
 fn file_watcher(filename: &Path) -> std::sync::mpsc::Receiver<notify::DebouncedEvent> {
     let (tx, rx) = channel();
-    watcher(tx, Duration::from_secs(1)).unwrap()
-        .watch(filename, RecursiveMode::NonRecursive).unwrap();
+    watcher(tx, Duration::from_secs(1))
+        .unwrap()
+        .watch(filename, RecursiveMode::NonRecursive)
+        .unwrap();
     rx
 }
 
 fn get_bounds(mesh: &stl_io::IndexedMesh) -> (Vector3<f32>, Vector3<f32>) {
-    let max_x = mesh.vertices.iter().map(|v| v[0]).max_by(|a, b| a.partial_cmp(b).unwrap()).expect("zero length mesh");
-    let max_y = mesh.vertices.iter().map(|v| v[1]).max_by(|a, b| a.partial_cmp(b).unwrap()).expect("zero length mesh");
-    let max_z = mesh.vertices.iter().map(|v| v[2]).max_by(|a, b| a.partial_cmp(b).unwrap()).expect("zero length mesh");
-    let min_x = mesh.vertices.iter().map(|v| v[0]).max_by(|b, a| a.partial_cmp(b).unwrap()).expect("zero length mesh");
-    let min_y = mesh.vertices.iter().map(|v| v[1]).max_by(|b, a| a.partial_cmp(b).unwrap()).expect("zero length mesh");
-    let min_z = mesh.vertices.iter().map(|v| v[2]).max_by(|b, a| a.partial_cmp(b).unwrap()).expect("zero length mesh");
-    (Vector3::new(min_x, min_y, min_z), Vector3::new(max_x, max_y, max_z))
+    let max_x = mesh.vertices
+        .iter()
+        .map(|v| v[0])
+        .max_by(|a, b| a.partial_cmp(b).unwrap())
+        .expect("zero length mesh");
+    let max_y = mesh.vertices
+        .iter()
+        .map(|v| v[1])
+        .max_by(|a, b| a.partial_cmp(b).unwrap())
+        .expect("zero length mesh");
+    let max_z = mesh.vertices
+        .iter()
+        .map(|v| v[2])
+        .max_by(|a, b| a.partial_cmp(b).unwrap())
+        .expect("zero length mesh");
+    let min_x = mesh.vertices
+        .iter()
+        .map(|v| v[0])
+        .max_by(|b, a| a.partial_cmp(b).unwrap())
+        .expect("zero length mesh");
+    let min_y = mesh.vertices
+        .iter()
+        .map(|v| v[1])
+        .max_by(|b, a| a.partial_cmp(b).unwrap())
+        .expect("zero length mesh");
+    let min_z = mesh.vertices
+        .iter()
+        .map(|v| v[2])
+        .max_by(|b, a| a.partial_cmp(b).unwrap())
+        .expect("zero length mesh");
+    (
+        Vector3::new(min_x, min_y, min_z),
+        Vector3::new(max_x, max_y, max_z),
+    )
 }
 
 fn get_center(bounds: (Vector3<f32>, Vector3<f32>)) -> Vector3<f32> {
@@ -64,9 +108,13 @@ fn get_center(bounds: (Vector3<f32>, Vector3<f32>)) -> Vector3<f32> {
 fn get_appropriate_scale(bounds: (Vector3<f32>, Vector3<f32>)) -> f32 {
     let diff = bounds.0 - bounds.1;
     let mut m = diff.x.abs();
-    if m > diff.y.abs() {m = diff.y.abs();}
-    if m > diff.z.abs() {m = diff.z.abs();}
-    return 1.0 / m
+    if m > diff.y.abs() {
+        m = diff.y.abs();
+    }
+    if m > diff.z.abs() {
+        m = diff.z.abs();
+    }
+    return 1.0 / m;
 }
 
 fn swap_mesh(w: &mut Window, mut c: &mut SceneNode, f: &Path) -> SceneNode {
